80fdc3810220ef28dbd185d2d42da32ede2defcd
vonvick <victor.nwaiwu9@gmail.com>
2016-10-25 22:06:55 +0100

    add books route and require firebase into the route

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var application = require('../app');
+var firebase = require('firebase');
+require('firebase/database');
+
+var db = firebase.database();
 
 
 router.use('/', application.isAuthenticated);
@@ -10,5 +14,17 @@ router.use('/', application.isAuthenticated);
 router.get('/', function(req, res) {
   res.render('user/index', {title: 'Users Page'});
 });
+
+router.get('/books', function(req, res) {
+  var ref = db.ref('users');
+  ref.on('value', function(snapshot) {
+    console.log(snapshot.val());
+    var users = snapshot.val();
+    res.render('user/books', {title: 'Books', users: users});
+  }, function (errorObject) {
+    console.log('The read failed: ' + errorObject.code);
+  });
+});
+
 
 module.exports = router;
