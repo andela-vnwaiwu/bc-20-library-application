39bb8e1b96f8c9c677be2e286c73df9ad00ae932
vonvick <victor.nwaiwu9@gmail.com>
2016-10-25 11:15:23 +0100

    add login and signup routes add login and signup routes  to the index page to handle show forms for authentication

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  res.render('index', {title: 'Home'});
 });
+
+router.get('/login', function (req, res) {
+  if(req.user) {
+    res.redirect('/');
+  } else {
+    res.render('login', {title: 'Login'});
+  }
+});
+
+router.get('/signup', function (req, res) {
+  if(req.user) {
+    res.redirect('/');
+  } else {
+    res.render('register', {title: 'Register'});
+  }
+});
+
+
 
 module.exports = router;
