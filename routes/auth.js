65398b2273cfd1cee31f4577cd87d2183480f641
vonvick <victor.nwaiwu9@gmail.com>
2016-10-25 15:21:47 +0100

    add authentication to the application and create database

--- routes/auth.js
+++ routes/auth.js
@@ -4,85 +4,84 @@ var router = express.Router();
 module.exports = function (firebase) {
   /* POST auth/login */
   router.post('/login', function (req, res) {
-    firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password).then(function (user) {
-      console.log(user);
-      console.log('I passed this test');
-      res.redirect('/');
-    }).catch(function(err) {
+    firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password)
+    .catch(function(err) {
       var errorCode = err.code;
       var errorMessage = err.message;
       if (errorCode === 'auth/wrong-password') {
         console.log('I didnt pass');
-        // alert('Wrong password.');
-      } else {
-        console.log('I don tire');
         res.redirect('/login');
-        // alert(errorMessage);
+      } else {
+        console.log(error.code);
+        res.redirect('/login');
       }
+    })
+    .then(function (user) {
+      res.redirect('/auth/success');
     });
   });
 
   /* POST auth/signup */
   router.post('/signup', function (req, res) {
-    firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password).then(function (user) {
-      console.log('I passed this test');
-      res.redirect('/');
-    }).catch(function(err) {
-      var errorCode = err.code;
-      var errorMessage = err.message;
-      if (errorCode === 'auth/wrong-password') {
-        console.log('I didnt pass');
-        // alert('Wrong password.');
-      } else {
-        console.log('I don tire');
-        // alert(errorMessage);
-      }
-    });
+    var email = req.body.email;
+    var password = req.body.password;
+    var userName = req.body.name;
+    var userArray = userName.split(' ');
+    var firstName = userArray[0];
+    var lastName = userArray[userArray.length-1];
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch(function (err) {
+        var errorCode = err.code;
+        var errorMessage = err.message;
+        if (errorCode === 'auth/email-already-in-use') {
+          console.log(errorCode);
+          res.redirect('/signup');
+        } else {
+          console.log('I don tire');
+          console.log(errorCode);
+          console.log(errorMessage);
+          res.redirect('/signup');
+        }
+      })
+      .then(function (user) {
+        if (user == firebase.auth().currentUser) {
+          console.log(user.uid);
+          user.updateProfile({
+            displayName: userName
+          }).then(function () {
+            console.log(user.displayName, user.email, user.role);
+          }).catch(function (error) {
+            console.log(error.code);
+          });
+          console.log(user.displayName, user.email, user.role);
+          return user;
+        }
+      })
+      .then(function(user) {
+        firebase.database().ref('users/' + user.uid).set({
+          firstname: firstName,
+          lastname: lastName,
+          email: email
+        });
+        console.log(user.displayName, user.email, user.role);
+        console.log('I passed this test');
+        res.redirect('/auth/success');
+      });
   });
 
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(function() {
-        var user = firebase.auth().currentUser;
-        user.updateProfile({
-          displayName: username
-        }).then(function() {
-            firebase.database().ref('users/' + user.uid).set({
-            username: username,
-            email: email
-          })
-        }, function(error) {
-          console.log(error.message);
-        })
-      })
-      .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // [START_EXCLUDE]
-        if (errorCode == 'auth/weak-password') {
-          alert('The password is too weak.');
-        } else {
-          alert(errorMessage);
-        }
-        console.log(error);
-        // [END_EXCLUDE]
-      });
-
-  // /* this route redirects the user to either the admin if he is an admin or users page*/
-  // router.get('/success', function (req, res) {
-  //   if(req.user && req.user.role === 'admin') {
-  //     res.redirect('/admin');
-  //   } else {
-  //     res.redirect('/issue');
-  //   }
-  // });
+  /* this route redirects the user to either the admin if he is an admin or users page*/
+  router.get('/success', function (req, res) {
+    var user = firebase.auth().currentUser;
+    if(user.email === 'victorjohn@yahoo.com') {
+      res.redirect('/admin');
+    } else {
+      res.redirect('/user');
+    }
+  });
 
   /* GET auth/logout */
   router.get('/logout', function (req, res) {
     firebase.auth().signOut();
-    // console.log(req.session.id);
-    // req.session.destroy();
-    // console.log(req.session);
     res.redirect('/login');
   });
 
