85ff9786c11bc23f26c40ad4d1aa9dbf2c27175d
vonvick <victor.nwaiwu9@gmail.com>
2016-10-26 23:14:59 +0100

    add the route for the user to borrow a book from the library

--- routes/user.js
+++ routes/user.js
@@ -26,5 +26,42 @@ router.get('/books', function(req, res) {
   });
 });
 
+router.get('/borrow/:title', function(req, res) {
+  var title = req.params.title;
+  var user = req.user.uid;
+  var ref = db.ref('books');
+  var borrow = db.ref('borrowed');
+  ref.orderByChild('title').equalTo(title).limitToFirst(1).once('child_added')
+    .then(function(snapshot) {
+      console.log('I have started');
+      var bookKey = snapshot.key;
+      var book = snapshot.val();
+      if (book.quantity > 1) {
+        borrow.push({
+          userid: user,
+          bookid: bookKey,
+          status: 'borrowed',
+          dateBorrowed: new Date().getTime(),
+          dateReturned: null,
+          dateDue: new Date().getTime() + 604800000
+        });
+        return db.ref('books/' + bookKey).update({
+          quantity: book.quantity -1
+        });
+      } else {
+          console.log('I am finished');
+          return res.redirect('/user/books');
+      }
+    })
+    .then(function () {
+      console.log('I finally made It');
+      res.redirect('/user/books');
+    })
+    .catch(function(error) {
+      console.log(error.code);
+      res.render('user/books', {title: 'Books'});
+    });
+});
+
 
 module.exports = router;
