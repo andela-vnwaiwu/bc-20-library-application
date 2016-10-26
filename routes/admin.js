d482b6f476d5dcd9c89ea60be6af19a337f2cb51
vonvick <victor.nwaiwu9@gmail.com>
2016-10-26 11:09:13 +0100

    add route to handle the creation of a book in the admin area.

--- routes/admin.js
+++ routes/admin.js
@@ -19,7 +19,7 @@ router.get('/', function(req, res) {
 /* GET admin/users 
   get the list of all users in the application
 */
-router.get('/users', function(req, res) {
+router.get('/books', function(req, res) {
   var ref = db.ref('users');
   // gets a snapshot of all users in the application
   ref.on('value', function(snapshot) {
@@ -31,34 +31,65 @@ router.get('/users', function(req, res) {
   });
 });
 
-router.post('/addbook', function(req, res) {
-  if(!req.form.isValid) {
-    res.render('admin/addbooks');
-  } else {
-    var title = req.body.title;
-    var author = req.body.author;
-    var description = req.body.description;
-    var isbn = req.body.isbn;
-    var quantity = req.body.quantity;
-    var category = req.body.category;
-    db.ref('books/').set({
-        title: title,
-        author: author,
-        description: description,
-        isbn: isbn,
-        quantity: quantity,
-        category: category
-      }).then(function(book) {
-        console.log('Books saved successfully')
-        res.redirect('/books');
-      }).catch(function(error) {
-        console.log(error.code);
-        res.render('admin/addbooks')
-      });
-    // res.redirect('admin/books');
-  }
-
+router.get('/addbook', function (req, res) {
+  var ref = db.ref('categories');
+  ref.once('value', function(snapshot) {
+    console.log(snapshot.val());
+    var categories = snapshot.val();
+    res.render('admin/addbook', {title: 'Books', categories: categories});
+  }, function (errorObject) {
+    console.log('The read failed: ' + errorObject.code);
+  });
 });
+
+router.post('/addbook', function(req, res) {
+  var title = req.body.title;
+  var author = req.body.author;
+  var description = req.body.description;
+  var isbn = parseInt(req.body.isbn);
+  var quantity = parseInt(req.body.quantity);
+  var category = req.body.category;
+  db.ref('books/').push({
+    title: title,
+    author: author,
+    description: description,
+    isbn: isbn,
+    quantity: quantity,
+    category: category
+  }).then(function(book) {
+    console.log('Books saved successfully')
+    res.redirect('/admin/books');
+  }).catch(function(error) {
+    console.log(error.code);
+    res.render('admin/addbook');
+  });
+});
+
+
+// router.post('/addbook', function(req, res) {
+//   var title = req.body.title;
+//   var author = req.body.author;
+//   var description = req.body.description;
+//   var isbn = req.body.isbn;
+//   var quantity = req.body.quantity;
+//   var category = req.body.category;
+//   db.ref('books/').push({
+//     title: title,
+//     author: author,
+//     description: description,
+//     isbn: isbn,
+//     quantity: quantity,
+//     category: category
+//   }).then(function(book) {
+//     console.log('Books saved successfully')
+//     res.redirect('/books');
+//   }).catch(function(error) {
+//     console.log(error.code);
+//     res.render('admin/addbooks')
+//   });
+//   // res.redirect('admin/books');
+
+// });
 
 
 router.get('/allcategories', function (req, res) {
