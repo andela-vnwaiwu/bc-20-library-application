098f0fed9db1c4b30dab54225fdff2bff91b71a8
vonvick <victor.nwaiwu9@gmail.com>
2016-10-27 01:42:14 +0100

    add the return book route in the user area

--- routes/user.js
+++ routes/user.js
@@ -26,8 +26,6 @@ router.get('/books', function(req, res) {
     .then(function (snapshot) {
       borrowed = snapshot.val();
       borrowedKey = snapshot.key;
-      // console.log(borrowed);
-      // console.log(books);
       for (var key in borrowed) {
         var bookId = borrowed[key].bookid;
         console.log(borrowed[key].bookid);
@@ -76,6 +74,39 @@ router.get('/borrow/:title', function(req, res) {
     .then(function () {
       console.log('I finally made It');
       res.redirect('/user/books');
+    })
+    .catch(function(error) {
+      console.log(error.code);
+      res.render('user/books', {title: 'Books'});
+    });
+});
+
+router.get('/return/:title', function(req, res) {
+  var title = req.params.title;
+  var user = req.user.uid;
+  var ref = db.ref('books');
+  var borrow = db.ref('borrowed');
+  ref.orderByChild('title').equalTo(title).limitToFirst(1).once('child_added')
+    .then(function(snapshot) {
+      console.log('I have started');
+      var bookKey = snapshot.key;
+      var book = snapshot.val();
+      console.log(bookKey);
+    //   borrow.push({
+    //     userid: user,
+    //     bookid: bookKey,
+    //     status: 'returned',
+    //     dateBorrowed: new Date().getTime(),
+    //     dateReturned: null,
+    //     dateDue: new Date().getTime() + 604800000
+    //   });
+    //   return db.ref('books/' + bookKey).update({
+    //     quantity: book.quantity -1
+    //   });
+    // })
+    // .then(function () {
+    //   console.log('I finally made It');
+    //   // res.redirect('/user/books');
     })
     .catch(function(error) {
       console.log(error.code);
