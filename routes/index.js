935cf4e4c6c4527cdd7884bbc03ee338a3b6fffd
vonvick <victor.nwaiwu9@gmail.com>
2016-10-27 19:53:25 +0100

    add categories view to the home route and views.

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,47 @@ var application = require('../app');
 var firebase = require('firebase');
 require('firebase/auth');
 
+var db = firebase.database();
+
 
 router.use('/', application.isLoggedIn);
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  if (req.user === null || req.user === undefined) {
-    console.log(req.user);
-    res.render('index', {title: 'Home', user: null});  
+  if (req.user === null) {
+    console.log(req.user)
+    var ref = db.ref('categories');
+    ref.once('value').then(function(snapshot) {
+      var categories = snapshot.val();
+      console.log(categories);
+      return categories;
+    }).then(function(categories){
+      console.log(req.user);
+      res.render('index', {title: 'Home', user: null, categories: categories});
+    })
+    .catch(function (errorObject) {
+      console.log(req.user)
+      console.log('The read failed: ' + errorObject.code);
+      res.render('error');
+    });
   } else {
-    console.log(req.user);
-    res.render('index', {title: 'Home', user: req.user});
+    var category = db.ref('categories');
+    category.once('value').then(function(snapshot) {
+      var categories = snapshot.val();
+      return categories;
+    }).then(function(categories){
+      console.log(req.user);
+      res.render('index', {title: 'Home', user: req.user, categories: categories});
+    })
+    .catch(function (errorObject) {
+      console.log('The read failed: ' + errorObject.code);
+      res.render('error');
+    }); 
   }
 });
 
+
+/* GET /login */
 router.get('/login', function (req, res) {
   if (req.user === null || req.user === undefined) {
     console.log(req.user);
@@ -28,12 +55,12 @@ router.get('/login', function (req, res) {
   }
 });
 
+
+/* GET /signup */
 router.get('/signup', function (req, res) {
   if (req.user === null || req.user === undefined) {
-    console.log(req.user);
     res.render('register', {title: 'Register', user: req.user});
   } else {
-    console.log(req.user);
     res.redirect('/');
   }
 });
